b1561cfc87fd3c7c8db5208501aaec28f90243d4
Marin Ivanov <metala@metala.org>
2013-04-05 15:02:20 +0300

    Hello world.

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
 jekyll-wikilinks-plugin
 =======================
 
-A plugin for Jekyll to process Wikipedia-like links in Maruku markdown
+A plugin for Jekyll to process Wikipedia style links in Maruku markdown. It supports the short `[[name]]` link syntax and also `[[name|Title]]`, where `name` is eiter a name of a page or of a post and `Title` is optional text that the link will show. If short syntax is used, the text for the link will be either the title of the page/post or the name of the page/post.
+
+Install
+-------
+
+Plugin should be placed in `_plugins` directory of the jekyll site root.
+
---
+++ wikilinks.rb
@@ -0,0 +1,68 @@
+module Jekyll
+  module Wikilinks
+    class Wikilink
+      def self.parse(text)
+        inner = text[2..-3]
+        name, title = inner.split('|', 2)
+        self.new(name, title)
+      end
+      
+      attr_accessor :name, :title
+      attr_reader :match
+      
+      def initialize(name, title)
+        @name = name.strip
+        @title = title
+      end
+      
+      def title
+        if @title.nil?
+          if not @match.nil? && @match.data.has?('title')
+            @match.data['title']
+          else
+            @name
+          end  
+        else
+          @title
+        end
+      end
+      
+      def url
+        @match.url
+      end
+      
+      def has_match?
+		not @match.nil?
+      end
+      
+      def match_post(posts)
+        @match = posts.find { |p| p.slug.downcase == @name.downcase }
+      end
+      
+      def match_page(pages)
+        @match = pages.find { |p| p.basename.downcase == @name.downcase  }
+      end
+      
+      def markdown
+        @match.nil? ? "\\[\\[#{title}\\]\\]" : "[#{title}](#{url})"
+      end
+    end
+  end
+  
+  module Convertible
+    alias old_transform transform
+
+    def transform
+      if converter.instance_of? MarkdownConverter
+        pat = /\[\[(.+?)\]\]/
+        @content = @content.gsub(pat) do |m|
+          wl = Wikilinks::Wikilink.parse(m)
+          wl.match_page(site.pages)
+          wl.match_post(site.posts) unless wl.has_match?
+          wl.markdown
+        end
+      end
+      old_transform
+    end
+  end
+end
